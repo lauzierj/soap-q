f7c4e34e3cc7d5da2f5d579b5b70e0deaa233e67
Matt Enlow <matt@novaugust.net>
2014-03-13 13:33:08 -0600

    It actually works now.

--- index.js
+++ index.js
@@ -14,17 +14,18 @@ var DEFAULTS = [
 
 function promisifyClient(client) {
     for (var prop in client) {
-        if ("function" === typeof client[prop] 
-            && defaults.indexOf(prop) == -1) {
+        if ("function" === typeof client[prop] && DEFAULTS.indexOf(prop) == -1) {
             var newName = prop + "Q";
-            client[newName] = function (args, options) {
-                var deferred = Q.defer();
-                client[prop](args, function (err, result) {
-                    if (err) deferred.reject(err);
-                    else deferred.resolve(result);
-                }, options);
-                return deferred.promise;
-            }
+            client[newName] = (function (oldName) {
+                return function (args, options) {
+                    var deferred = Q.defer();
+                    client[oldName](args, function (err, result) {
+                        if (err) deferred.reject(err);
+                        else deferred.resolve(result);
+                    }, options);
+                    return deferred.promise;
+                }
+            })(prop);
         }
     }
 }
@@ -32,9 +33,9 @@ module.exports = function (soap) {
     soap = soap || require("soap");
     soap.createClientQ = function (url, cb) {
         var deferred = Q.defer();
-        soap.createClient(url, function (err, client) {
+        this.createClient(url, function (err, client) {
             if (err) return deferred.reject(err);
-            client = promisifyClient(client);
+            promisifyClient(client);
             deferred.resolve(client);
         })
         return deferred.promise;
